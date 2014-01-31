9283ff9ffe66baae7721c9955b14df0b9b108977
dyep49 <dyep49@gmail.com>
2014-01-31 15:03:38 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeMidpoint::Application.config.secret_token = '0c164c5a1b6ebae70e50cb2b9e47a07c6de3d0196ebd4cf7459ca24550bd2a351338edf5d95075b411a20b009bc2891a9aa342a47f8437a4fe963b6f25826ce9'
