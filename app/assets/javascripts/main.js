b4a6c85f509ddb9c0afc3eeab1e0e4deddbae0bf
jtrope89 <jonathan@jonathantrope.com>
2014-02-03 16:32:37 -0500

    testing geocoder and adding a partial for results map

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,39 @@
+var app = app || {
+    initialize: function() {
+        app.locations = [];
+        app.views = [];
+        app.constants = {
+            // Designated the maximum amount of locations that
+            //can be added for midpoint calculation  (excluding user's location)
+            MAX_LOCATION_INPUTS: 2
+        };
+
+        app.elements = {
+            $add_location_btn: $("#add_location_btn"),
+            $non_user_location_inputs: $("#non_user_location_inputs"),
+            $results_map_div: $("#results_map")
+        };
+        app.addEventListeners();
+    },
+
+    addEventListeners: function() {
+        // Adds a new location input box
+        app.elements.$add_location_btn.on("click", function(e) {
+            e.preventDefault(); // prevent page from refreshing and such
+            var number_of_inputs = app.elements.$non_user_location_inputs.children().length;
+
+            if ( number_of_inputs < app.constants.MAX_LOCATION_INPUTS ) {
+                var $first_destination_input =  app.elements.$non_user_location_inputs.children().first();
+                var $new_location_input = $first_destination_input.clone();
+                $new_location_input.attr( "placeholder", "Enter Destination " + ( number_of_inputs + 1 ) );
+                app.elements.$non_user_location_inputs.append( $new_location_input );
+            } else {
+                alert("Cannot add more locations");
+            }
+        });
+    }
+};
+
+$(function() {
+    app.initialize();
+});
