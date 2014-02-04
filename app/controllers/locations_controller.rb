5ca15a9ecfbda73062f132fd4b939c7eb819adab
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-02-04 15:51:17 -0500

    added location.js crud stuff

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,38 +1,19 @@
-class YelpLocationsController < ApplicationController
+class LocationsController < ApplicationController
 
   def index
     #get
+
       respond_to do |format|
       puts params
-      format.html { @locations = YelpLocation.all }
-      format.json { render json: YelpLocation.all }
+      format.html { @locations = Location.all }
+      format.json { render json: Location.all }
     end
   end 
 
   def create
     
-     location = YelpLocation.create(params[:location])
+     location = Location.create(params[:location])
        render json: location
   end 
-
-  def new 
-    #get
-  end 
-
-  def edit
-    #get
-  end
-
-  def show
-    #get
-  end 
-
-  def update
-    #put
-  end 
-
-  def destroy
-    #delete
-  end
 
 end
---
+++ app/controllers/yelp_locations_controller.rb
@@ -0,0 +1,38 @@
+class YelpLocationsController < ApplicationController
+
+  def index
+    #get
+      respond_to do |format|
+      puts params
+      format.html { @locations = YelpLocation.all }
+      format.json { render json: YelpLocation.all }
+    end
+  end 
+
+  def create
+    
+     location = YelpLocation.create(params[:location])
+       render json: location
+  end 
+
+  def new 
+    #get
+  end 
+
+  def edit
+    #get
+  end
+
+  def show
+    #get
+  end 
+
+  def update
+    #put
+  end 
+
+  def destroy
+    #delete
+  end
+
+end
