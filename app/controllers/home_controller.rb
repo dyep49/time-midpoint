599dcee44675967f93085c113e07ccd6e60878fd
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-02-06 23:38:17 -0500

    yelp buttons, heeeyyyoooo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,21 @@ class HomeController < ApplicationController
   	if user_signed_in? == false
   		redirect_to '/landing'
   	end
-  	
   end
+
+def text
+    account_sid = 'ACa597f6f6c4c7b6e3b05f67b16aa34db2' 
+    auth_token = '3d7e331e3cc85cc07243a27e34a7c487' 
+     
+    # set up a client to talk to the Twilio REST API 
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+     
+    @client.account.messages.create({
+      :from => '+14109414506',    
+      :to => params["text"]["contact_num"],
+      :body => params["text"]["message"]
+    })
+    render json: "{}"
+end 
 
 end
